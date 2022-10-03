82e44cce1c080453517baa76bae3c3ceb9a32437
xaviguasch <xaviguasch@gmail.com>
2022-10-03 12:20:09 +0200

    styled Hero component for mobile view

--- components/hero/hero.js
+++ components/hero/hero.js
@@ -1,17 +1,24 @@
 import Link from 'next/link'
 
+import classes from './hero.module.css'
+
 function Hero({ heroCopy }) {
   return (
     <div
+      className={`${classes.Hero} h-padding`}
       style={{
         backgroundImage: `url(${heroCopy.backgroundImg.mobile})`,
       }}
     >
-      <div className='copy'>
-        <span>{heroCopy.tag}</span>
-        <h1>{heroCopy.productName}</h1>
-        <p>{heroCopy.productCopy}</p>
-        <Link href='/'>{heroCopy.buttonText}</Link>
+      <div className={classes.copy}>
+        <div className='title-area'>
+          <span className='tag helper-text-diluted-white'>{heroCopy.tag}</span>
+          <h1 className='title title--l helper-text-white'>{heroCopy.productName}</h1>
+        </div>
+        <p className='text helper-text-semi-diluted-white'>{heroCopy.productCopy}</p>
+        <Link href='/'>
+          <a className='button'> {heroCopy.buttonText}</a>
+        </Link>
       </div>
     </div>
   )
