af4848fa8b6026ff47f7e4a39f3bc3852f363b3b
xaviguasch <xaviguasch@gmail.com>
2022-10-30 01:10:14 +0200

    styled PDGallery component for tablet view

--- components/product-display/pd-gallery.js
+++ components/product-display/pd-gallery.js
@@ -7,9 +7,22 @@ function PDGallery({ productInfo }) {
 
   return (
     <div className={classes.PDGallery}>
-      <img className={classes.gridImg} src={gallery.first.mobile} alt='' />
-      <img className={classes.gridImg} src={gallery.second.mobile} alt='' />
-      <img className={classes.gridImg} src={gallery.third.mobile} alt='' />
+      <picture className={classes.picA}>
+        <source srcSet={gallery.first.desktop} media='(min-width: 900px)' alt='' />
+        <source srcSet={gallery.first.tablet} media='(min-width: 600px)' alt='' />
+        <img className={classes.gridImg} src={gallery.first.mobile} alt='' />
+      </picture>
+      <picture className={classes.picB}>
+        <source srcSet={gallery.second.desktop} media='(min-width: 900px)' alt='' />
+        <source srcSet={gallery.second.tablet} media='(min-width: 600px)' alt='' />
+
+        <img className={classes.gridImg} src={gallery.second.mobile} alt='' />
+      </picture>
+      <picture className={classes.picC}>
+        <source srcSet={gallery.third.desktop} media='(min-width: 900px)' alt='' />
+        <source srcSet={gallery.third.tablet} media='(min-width: 600px)' alt='' />
+        <img className={classes.gridImg} src={gallery.third.mobile} alt='' />
+      </picture>
     </div>
   )
 }
